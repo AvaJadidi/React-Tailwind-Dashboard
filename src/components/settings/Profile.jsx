cd67ce2c1134f2098621aadd58f8b87b3865c7b4
ava jadidi <a.jadidi@ymail.com>
2025-01-11 13:35:56 -0800

    add favicon and title ...

--- src/components/settings/Profile.jsx
+++ src/components/settings/Profile.jsx
@@ -6,13 +6,13 @@ function Profile() {
     <SettingSection icon={User} title={"Profile"}>
       <div className="flex flex-col sm:flex-row items-center mb-6">
         <img
-          src="https://randomuser.me/api/portraits/men/3.jpg"
+          src="public/about.jpg"
           alt="Profile"
           className="rounded-full w-20 h-20 object-cover mr-4"
         />
         <div>
-          <h3 className="text-lg font-semibold text-gray-100">John Doe</h3>
-          <p className="text-gray-400">john.doe@example.com</p>
+          <h3 className="text-lg font-semibold text-gray-100">Ava Jadidi</h3>
+          <p className="text-gray-400">a.jadidi@ymail.com</p>
         </div>
       </div>
         <button className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded transition duration-200 w-full sm:w-auto">Edit Profile</button>
